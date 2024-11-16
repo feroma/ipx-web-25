b0309e170f416b030484fb8d08af9a60e12f4f60
feroma <federico.romanazzo@gmail.com>
2024-11-16 08:45:49 +0100

    API #introducing sections

--- api/index.php
+++ api/index.php
@@ -4,28 +4,38 @@ header( "Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, A
 header( 'Content-Type: application/json' );
 
 
-
 $response = [
 
-	"nav"=>[
-        ["id"=>"1", "label"=> 'homepage', "slug"=>'/'],
-        ["id"=>"2", "label"=> 'About Us', "slug"=>'/about-us'],
-        ["id"=>"3", "label"=> 'Contact', "slug"=>'/contact'],
-    ],
-    "pages"=>[
-		"1"=>[
-			"title"=> 'Welcome',
-            "body"=> 'Welcome to our new site'
-        ],
-        "2"=>[
-			"title"=> 'About us',
-            "body"=> 'Here some information about our company'
-        ],
-        "3"=>[
-			"title"=> 'Contact',
-            "body"=> 'Let\'s keep in touch!'
-        ],
-    ]
+	"nav" => [
+		[ "id" => "1", "label" => 'homepage', "slug" => '/' ],
+		[ "id" => "2", "label" => 'About Us', "slug" => '/about-us' ],
+		[ "id" => "3", "label" => 'Contact', "slug" => '/contact' ],
+	],
+	"pages" => [
+		"1" => [
+			"title" => 'Welcome',
+			"body" => 'Welcome to our new site',
+			"sections" =>[
+				'1' => [
+					"title" => 'First Section',
+					"body" => 'This is the first section',
+				],
+				'2' => [
+					"title" => 'Second Section',
+					"body" => 'This is the second section',
+				]
+
+			]
+		],
+		"2" => [
+			"title" => 'About us',
+			"body" => 'Here some information about our company',
+		],
+		"3" => [
+			"title" => 'Contact',
+			"body" => 'Let\'s keep in touch!',
+		],
+	],
 
 ];
 
