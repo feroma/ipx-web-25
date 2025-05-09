828ed1c8afa4b98ccb9b5bfa2d3e6bbc14c1fa69
feroma <federico.romanazzo@gmail.com>
2025-05-09 07:07:46 +0200

    FE update

--- components/WidgetTeaserList1.vue
+++ components/WidgetTeaserList1.vue
@@ -15,14 +15,30 @@ const {config} = useAppState()
          class="teaser"
          :class="teaser.props.class || ''"
     >
+
+
       <div class="teaser-image">
         <svg width="309" height="320" viewBox="0 0 309 320" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M218.853 320H5.99063C2.68058 320 -0.00418842 317.319 -0.00936037 314.009L-0.49061 6.00938C-0.495794 2.69201 2.192 0 5.50937 0L302.512 0C305.821 0 308.506 2.67875 308.512 5.98762L308.974 229.749C308.991 237.721 305.833 245.372 300.199 251.012L240.078 311.201C234.451 316.835 226.815 320 218.853 320Z"/>
+          <defs>
+            <mask :id="'myMask_'+teaser_count">
+              <rect width="100%" height="100%" fill="black"/>
+              <path fill="white" d="M218.853 320H5.99063C2.68058 320 -0.00418842 317.319 -0.00936037 314.009L-0.49061 6.00938C-0.495794 2.69201 2.192 0 5.50937 0L302.512 0C305.821 0 308.506 2.67875 308.512 5.98762L308.974 229.749C308.991 237.721 305.833 245.372 300.199 251.012L240.078 311.201C234.451 316.835 226.815 320 218.853 320Z"/>
+            </mask>
+          </defs>
+          <image
+
+              :href="config.REPO_URL+'/'+teaser.props.image.src"
+              width="309"
+              height="320"
+              :mask="'url(#myMask_'+teaser_count+')'"
+          />
         </svg>
-        <NuxtImg
-            v-if="teaser.props.icon"
-            :alt="teaser.props.icon.alt"
-            :src="config.REPO_URL+'/'+teaser.props.icon.src"/>
+
+
+<!--        <NuxtImg-->
+<!--            v-if="teaser.props.image"-->
+<!--            :alt="teaser.props.image.alt"-->
+<!--            :src="config.REPO_URL+'/'+teaser.props.image.src"/>-->
       </div>
       <div class="teaser-entry">
         <h4 v-html="teaser.props.title"/>
